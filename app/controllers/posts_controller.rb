78b9017584776981298034c80f0395ad4acffbf8
Rashed Arman <rashedarman21@gmail.com>
2023-07-22 04:39:36 +0600

    feat: update controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
   def index
     @user_id = params[:user_id]
+    @user = User.find(@user_id)
   end
 
   def show
     @user_id = params[:user_id]
-    @post_id = params[:id]
+    @id = params[:id]
+
+    @user = User.find(@user_id)
+    @post = Post.find(@id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all.order(id: :asc)
+  end
 
   def show
     @user_id = params[:id]
+    @user = User.find(@user_id)
   end
 end
