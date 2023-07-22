9c67ada4ae9e77067447d12aeaaa17130667beb1
Rashed Arman <rashedarman21@gmail.com>
2023-07-23 03:15:28 +0600

    feat: create new post controller method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,27 @@ class PostsController < ApplicationController
     @user = User.find(@user_id)
     @post = Post.find(@id)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.author = current_user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+
+    respond_to do |f|
+      if @post.save
+        f.html { redirect_to user_posts_path }
+      else
+        f.html { render :new }
+      end
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
