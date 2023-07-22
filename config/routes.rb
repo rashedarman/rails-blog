ed3f8d37492c0a86f094e0c20d5830216e52a8b9
Rashed Arman <rashedarman21@gmail.com>
2023-07-23 03:17:50 +0600

    feat: add comment and likes route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get 'likes/new'
+  get 'comments/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
  
-  root 'users#index'
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  root "users#index"
+  resources :users do
+    resources :posts do
+      resources :comments, :likes
+    end
   end
 end
