b31babaed44f7d2d8b8f4c70a99f650a0e960d04
Rashed Arman <rashedarman21@gmail.com>
2023-07-29 03:40:03 +0600

    feat: setup devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'likes/new'
-  get 'comments/new'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
- 
+  devise_for :users
   root "users#index"
   resources :users do
     resources :posts do
