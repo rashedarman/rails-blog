0b78fe6375d8aa594bd822d70d6e6f87c5d6bfe5
Rashed Arman <rashedarman21@gmail.com>
2023-07-29 19:14:50 +0600

    feat: set default column values

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable,
+         :validatable, :confirmable
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
+
+  attribute :posts_counter, :integer, default: 0
+  attribute :photo, :string, default: 'https://robohash.org/hicveldicta.png'
 
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
